f9490d77cd1469754c0fe715eda63b449e4c8ffb
margusp2school <101623179+margusp2school@users.noreply.github.com>
2022-03-15 09:49:44 +0200

    Update tund2-arrays.py

--- tund2-arrays.py
+++ tund2-arrays.py
@@ -1,12 +1,35 @@
 # https://www.w3schools.com/python/python_conditions.asp
-numbrid = [1, 2, 3, 4, 65, 6, 7, 8, 9, 10, 99999]
+numbrid = []
+
+# 1. Küsida, mitu vanust tahad sisestada
+# 2. Nende vanuste keskmine välja printida
+
+mitu_vanust = int(input("Mitu vanust soovid sisestada?: "))
+
+for i in range(0, mitu_vanust):
+    number = int(input("Number " + str(i) + ": "))
+    numbrid.append(number)
 
 summa = 0
 for x in numbrid:
     summa = summa + x 
-print(summa)
+print(summa / mitu_vanust)
+
 
 # Ülesanded
 # 1. Prindi välja kõige suurem number
 # 2. Prindi välja kõige väiksem number
 # Kasuta abiks: https://www.w3schools.com/python/python_conditions.asp
+
+
+# Summa
+# summa = 0
+# for x in numbrid:
+#     summa = summa + x 
+# print(summa)
+
+# k6ige_suurem_number = 0
+# for x in numbrid:
+#     if x > k6ige_suurem_number:
+#         k6ige_suurem_number = x 
+# print(k6ige_suurem_number)
