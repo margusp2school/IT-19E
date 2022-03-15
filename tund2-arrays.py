7b107027644dca1061fd0c317fb0dc122f921c9c
margusp2school <101623179+margusp2school@users.noreply.github.com>
2022-03-15 09:18:24 +0200

    Create tund2-arrays.py

---
+++ tund2-arrays.py
@@ -0,0 +1,12 @@
+# https://www.w3schools.com/python/python_conditions.asp
+numbrid = [1, 2, 3, 4, 65, 6, 7, 8, 9, 10, 99999]
+
+summa = 0
+for x in numbrid:
+    summa = summa + x 
+print(summa)
+
+# Ülesanded
+# 1. Prindi välja kõige suurem number
+# 2. Prindi välja kõige väiksem number
+# Kasuta abiks: https://www.w3schools.com/python/python_conditions.asp
