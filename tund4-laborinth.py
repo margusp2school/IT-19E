0c9eda96b8f5a91f165c93d1c5161e33e438d88d
margusp2school <101623179+margusp2school@users.noreply.github.com>
2022-04-05 09:17:47 +0300

    Update tund4-laborinth.py
    sae

--- tund4-laborinth.py
+++ tund4-laborinth.py
@@ -9,6 +9,13 @@ map = [
 start_x = 0
 start_y = 0
 
+def saab_liikuda_paremale(map, praegused_kordinaadid):
+    print("SIIN TOIMIB SEE")
+    print(map[0][0])
+    print("SIIN TOIMUS SEE")
+    return False 
+
+print(saab_liikuda_paremale(map, [start_x, start_y]))
 
 def print_kaart():
     print(map[0])
@@ -16,6 +23,5 @@ def print_kaart():
     print(map[2])
     print(map[3])
     print(map[4])
-
 
 print_kaart()
