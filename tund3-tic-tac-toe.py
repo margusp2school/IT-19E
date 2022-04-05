3d524d458533360a7b740085c3216db166c5b733
margusp2school <101623179+margusp2school@users.noreply.github.com>
2022-04-05 09:02:37 +0300

    salvestan staatus

---
+++ tund3-tic-tac-toe.py
@@ -0,0 +1,3 @@
+if 1 == 1: 
+    if 2 == 2:
+        print("tEre")
---
+++ tund4-laborinth.py
@@ -0,0 +1,21 @@
+map = [
+    [12, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 24]
+]
+
+start_x = 0
+start_y = 0
+
+
+def print_kaart():
+    print(map[0])
+    print(map[1])
+    print(map[2])
+    print(map[3])
+    print(map[4])
+
+
+print_kaart()
